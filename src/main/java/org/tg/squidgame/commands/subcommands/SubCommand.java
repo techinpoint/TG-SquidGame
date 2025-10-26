b1ccc6299f667626bdde029d8fc1a99a06ee92f1
techinpoint-YT <claritycore1@gmail.com>
2025-10-26 19:28:44 +0545

    Delete src/main directory

--- src/main/java/org/tg/squidgame/commands/subcommands/SubCommand.java
+++
@@ -1,7 +0,0 @@
-package org.tg.squidgame.commands.subcommands;
-
-import org.bukkit.command.CommandSender;
-
-public interface SubCommand {
-    boolean execute(CommandSender sender, String[] args);
-}
